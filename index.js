367458c10818a5e88236aa6dd0975429ed257dbf
mathopoulos <alexandros@mathopoulos.com>
2023-05-23 22:58:38 +0000

    Fixed a few bugs.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //------------------------------------------------------------------------------------------------------------
 //User Editable Configurable Value
 //Below are four variables you can edit to easily customize the script.
+const runOnlyOnce = true; // set to true to run the script one time only and then exit
 
-const runOnlyOnce = true; // set to true to run the script one time only and then exit. Set to false to run periodically based in the intervalDays value below
-
-const intervalDays = 30; // set the number of days between script runs if runOnlyOnce is false.
+const intervalDays = 30; // set the number of days between script runs if runOnlyOnce is false
 
 const daysSinceLastActive = 90; //set this to the maximum number of days since last access that a member can have to be considered for an Enterprise seat. Seats will be given to users who have been since the las X days. 
+// set the batch count to be retrieved in each batch. The default value is 5.
+const batchCount = 5;
 
-const batchCount = 5; // set the batch count to be retrieved in each batch. We recommend running this script with a batch value of 5 to ensure that it works then stopping the script and changing the batch value to 100. 
 
 //------------------------------------------------------------------------------------------------------------
 //REQUIRED authintication credentials
@@ -22,10 +22,10 @@ const enterpriseId = 'YOURENTERPRISEID'; //Enter the ID of the Trello Enterprise
 //------------------------------------------------------------------------------------------------------------
 //Below this line is the main execution code. Edits below this line are not recommended unless you are trying to adapt the core funtionality of the script.
 
+const headers = { 'Accept': 'application/json' };
 const request = require('request');
 const moment = require('moment');
 const process = require('process');
-const headers = { 'Accept': 'application/json' };
 
 let membersAssigned = 0;
 let membersSkipped = 0;
@@ -46,7 +46,7 @@ function processNextBatch() {
     const membersResponse = body;
     console.log(`Pulled our batch of ${membersResponse.length} members. Starting to give them Enterprise seats now...`);
     if (!Array.isArray(membersResponse) || membersResponse.length === 0) {
-      console.log("No more members to process");
+      console.log("No more members to process, All done!");
       return;
     }
     membersResponse.forEach((member) => {
@@ -59,6 +59,8 @@ function processNextBatch() {
           headers: headers,
           form: data
         }, (error, response, body) => {
+          //console.log(body);
+          //console.log(member.username);
           const licensedResponse = JSON.parse(body);
           membersAssigned += 1;
           console.log(`Gave an Enterprise Seat to member: ${member.username}. Have now assigned a total of ${membersAssigned} Enterprise seats.`);
@@ -77,7 +79,7 @@ function processNextBatch() {
 if (runOnlyOnce) {
   console.log('Running script one time only');
   processNextBatch();
-  process.exit(0);
+
 } else {
   console.log(`Running script automatically every ${intervalDays} days`);
   cron.schedule(`0 0 1 */${intervalDays} * *`, () => {
